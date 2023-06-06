f79a5aec1eee991d1837744a409ca21af9bca971
Mamerto Fabian Jr <mamerto@codefrost.com>
2023-06-06 08:32:32 -0400

    add streamlit app

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 ## Set up
 
 ### Mirroring the website
-
-#### Mirror the blog site
 ```
 wget --mirror --convert-links --adjust-extension --page-requisites --no-parent https://python./en/latest/index.html
 ```
 
 ### Creating the Indexes
-Execute the notebook `create_indexes.ipynb` to create the indexes for the blog, main and help sites.
+Execute the notebook `create_indexes.ipynb` to create the indexes for the mirrored website.
 This will also create a merged index for all three sites and saved in the `index` folder.
---
+++ streamlit_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from streamlit_chat import message
+
+from chatbot import ask_bot, get_sources
+
+# ------------------------------------------------------------------
+# Save history
+
+# This is used to save chat history and display on the screen
+if 'answer' not in st.session_state:
+    st.session_state['answer'] = []
+
+if 'question' not in st.session_state:
+    st.session_state['question'] = []
+
+st.title("What question do you have for LangChain Docs?")
+prompt = st.text_input("Your question")
+
+if prompt:
+    result = ask_bot(prompt)
+
+    answer = result['answer']
+    sources = get_sources(result)
+
+    # add the sources to the answer
+    response = f"{answer}\n\nSource(s):\n {sources}"
+
+    # Add the question and the answer to display chat history in a list
+    # Latest answer appears at the top
+    st.session_state.question.insert(0, prompt)
+    st.session_state.answer.insert(0, response)
+
+    # Display the chat history
+    for i in range(len( st.session_state.question)) :
+        message(st.session_state['question'][i], is_user=True, key=f"question{i}")
+        message(st.session_state['answer'][i], is_user=False, key=f"answer{i}")
